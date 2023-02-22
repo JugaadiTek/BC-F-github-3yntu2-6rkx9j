4dc1f62f695e986a0094e7c9682d148a9ca6b176
paurush rai <paurush@modocosm.io>
2023-02-22 16:53:13 +0530

    nav +footer css test

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from "./router";
 import "modern-normalize";
 import "./stylesheet/main.css";
 
+
+// import "./stylesheet/.main.old.css";
+
 const app = createApp(App);
 
 app.use(router);
