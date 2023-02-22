e0a3713c0947d8011fcc130c0c7467b695e949a2
paurush rai <paurush@modocosm.io>
2023-02-22 16:26:45 +0530

    added navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,49 @@
 <script setup>
+import NavBar from '@/components/MC/NavBar.vue';
+import { ref } from 'vue';
+
+
+const navigations = ref([
+  {
+    component: "NavBar",
+    logo: {
+      type: "svg/img/text",
+      text: "",
+      altText: "",
+      logoFill: "",
+    },
+    alt: ["nogap", "darkbg"],
+    bricks: [
+      { label: "Solution", url: '' },
+      { label: "Resources", url: '' },
+      { label: "About Us", url: '' }
+    ],
+    buttons: [
+      {
+        type: "",
+        text: "",
+        url: "",
+        alt: [""],
+        external: true,
+        rel: [""]
+      }
+    ]
+  }
+])
+
 </script>
 
 <template>
   <header>
-    <nav class="container">
+    <nav class="container mainNav nogap darkbg" v-for="navigationbar in navigations" >
       <!--- Here ----->
+      <NavBar :navbtn="navigationbar.bricks" />
     </nav>
   </header>
 </template>
+
+  
+<style scoped>
+
+
+</style>
