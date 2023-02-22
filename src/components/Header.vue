4d39c219b642c5bd4f11c939e1732dd313ae1096
Jugaadi <william@modocosm.io>
2023-02-22 16:03:46 +0530

    Footer and Nav Prep
    asdf

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,10 @@
 <script setup>
-import IconMonospace from "./icons/Monospace.vue";
 </script>
 
 <template>
   <header>
     <nav class="container">
-      <a href="/">
-        <IconMonospace />
-      </a>
-      <span class="header__description">
-        An example site powered by <span class="accent">Vue</span> and
-        <span class="accent">Directus</span>.
-      </span>
+      <!--- Here ----->
     </nav>
   </header>
 </template>
