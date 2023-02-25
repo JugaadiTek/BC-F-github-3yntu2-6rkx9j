1e12711276727e1056aff46221794285fae1ffdf
paurush rai <paurush@modocosm.io>
2023-02-26 00:46:50 +0530

    Revert "nav +footer css test"
    This reverts commit 4dc1f62f695e986a0094e7c9682d148a9ca6b176.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import router from "./router";
 import "modern-normalize";
 import "./stylesheet/main.css";
 
-
-// import "./stylesheet/.main.old.css";
-
 const app = createApp(App);
 
 app.use(router);
