d33c7d52484b4f7006c84db7ddadf0bbf7272fb0
paurush rai <paurush@modocosm.io>
2023-02-26 19:50:13 +0530

    hero working

--- src/components/MC/hero.vue
+++ src/components/MC/hero.vue
@@ -5,31 +5,32 @@ defineProps({
   sliceType: String,
 })
 
+
 </script>
 <template>
   <div class="codecont">
     <div class="codeloop">
       <h1>Hero</h1>
-      <pre class="title">{{ JSON.stringify(herocont) }}</pre>
+      <pre class="title">{{ JSON.parse(herocont) }}</pre>
       <code>{{ setSliceVars(herocont) }}</code>
     </div>
   </div>
 
 
   <div class="inner-container">
-    <img class="hero" :src="herocont.bricks.heroBG" :alt="herocont.bricks.AltText">
+    <img class="hero" :src="JSON.parse(herocont).bricks.heroBG" :alt="JSON.parse(herocont).bricks.AltText">
     <div class="content">
       <div class="content-block">
         <div>
-          <img class="hero" :src="herocont.bricks.heroBG" :alt="herocont.bricks.AltText">
-          <h2>{{ herocont.bricks.title }}</h2>
-          <p>{{ herocont.bricks.description }}</p>
-          <span v-for="(atom, index) in herocont.bricks.atoms" :key="index">
+          <img class="hero" :src="JSON.parse(herocont).bricks.heroBG" :alt="JSON.parse(herocont).bricks.AltText">
+          <h2>{{JSON.parse(herocont).bricks.title}}</h2>
+          <p>{{JSON.parse(herocont).bricks.description}}</p>
+          <span v-for="(atom, index) in JSON.parse(herocont).bricks.title" :key="index">
             <a :class="[atom.type, atom.variant, atom.styleMods]" :href="atom.url">{{ atom.text }}</a>
           </span>
         </div>
         <div>
-          <img v-for="(image, index) in herocont.bricks.imageList" :src="image.imageurl" :alt="image.alt">
+          <!-- <img v-for="(image, index) in herocont.bricks.imageList" :src="image.imageurl" :alt="image.alt"> -->
         </div>
       </div>
     </div>
