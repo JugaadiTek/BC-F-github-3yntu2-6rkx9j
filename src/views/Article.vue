bbfc12fba63de18608b4811c6acd8d6bcc6554bc
Jugaadi <william@modocosm.io>
2023-02-26 18:08:11 +0530

    a

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -2,9 +2,21 @@
 import { ref } from 'vue';
 import { directus } from '@/services/directus';
 import { RouterLink, useRoute, useRouter } from 'vue-router';
-// import { getAssetURL } from '@/utils/get-asset-url';
+import { getAssetURL } from '@/utils/get-asset-url';
 import sliceLogic from '@/utils/sliceLogic.vue';
-
+// =========================  COMPONENTS  ==========================================================
+// import hero from "@/components/MC/hero.vue";
+// import QueryString from 'qs';
+// import { KeyObject } from 'crypto';
+//import servicescard from "@/components/MC/servicescard.vue";
+//import imageWithContentList from "@/components/MC/imageWithContentLIst.vue";
+//import imgSlider from "@/components/MC/imgSlider.vue";
+//import ContentPairsRepeater from "@/components/MC/ContentPairsRepeater.vue";
+//import ctaHero from "@/components/MC/ctaHero.vue";
+//import artcont from "@/components/MC/artcont.vue";
+//import postfeed from "@/components/MC/postfeed.vue";
+//import newsletter from "@/components/MC/newsletter.vue";
+// =================================================================================================
 const router = useRouter();
 const route = useRoute();
 const article = ref(null);
@@ -138,30 +150,30 @@ function removeFirstLastChar(str) {
 //================================================================================================
 //============function to create a new object ====================================================
 //================================================================================================
-let json
+let newobj = {}
 function createNewObject(sliceobj) {
-  json = {
+  newobj = {
     bump: [
     JSON.parse(sliceobj)
     ]
   }
-  constant sliceobj.replace(/\\n/g, ' ');
   // sliceobj= sliceobj.bump[0]
   // ctype=obj.bump[0].component
-  return json.replace(sliceobj.bump[0])
-  // return sliceobj.bump[0]
+  console.log(newobj.bump[0])
+  return newobj.bump[0]
 }
+
 // get values from object
 </script>       
 <template>
   <!---============================================================================================================================================================================================================-->
   <!------------------------------------------------ MODOCOSM SLICE MASTER ---------------------------------------------------------------------------------------------------------------------------------------------------->
   <!---============================================================================================================================================================================================================-->
-
+  
   <section v-for="(slice, index) in article.grab_a_slice" >
     <sliceLogic 
     :sliceType="createNewObject(slice.nosql_datastore_id.json_datastore).component" 
-    :sliceData="createNewObject(slice.nosql_datastore_id.json_datastore)" 
+    :sliceData="JSON.stringify(createNewObject(slice.nosql_datastore_id.json_datastore))" 
     :pindex="index" />
   </section>
 
