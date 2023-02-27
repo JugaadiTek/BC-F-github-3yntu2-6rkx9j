0a557669a20a98103840025f101d7d207b539a5b
paurush rai <paurush@modocosm.io>
2023-02-27 16:02:36 +0530

    Update artcont.vue

--- src/components/MC/artcont.vue
+++ src/components/MC/artcont.vue
@@ -2,30 +2,30 @@
 import { imagePrefix } from '@/utils/imgurlPrefix.js';
 
 defineProps({
-  data: {Object},
+  data: { Object },
   preImgUrl: String,
   sliceType: String,
-  
+
 })
 </script>
 
 
 <template>
-  <!-- <div class="codecont">
-    <div class="codeloop">
-      <h1>art cont</h1>
-      <pre class="title">{{ JSON.parse(data) }}</pre>
-    </div>
-  </div> -->
-  <div class="inner-container">
-    <div class="content">
-      <div class="content-block">
-        <h2>{{ JSON.parse(data).bricks.title }}</h2>
-        <p> {{ JSON.parse(data).bricks.description }} </p>
-        <p v-if="JSON.parse(data).bricks.description2 ==! ' ' " > {{ JSON.parse(data).bricks.description2 }} </p>
+  <div class="mcFix">
+    <div class="container">
+      <div class="inner-container">
+        <div class="content">
+          <div class="content-block">
+            <h2>{{ JSON.parse(data).bricks.title }}</h2>
+            <p> {{ JSON.parse(data).bricks.description }} </p>
+            <p v-if="JSON.parse(data).bricks.description2 == ! ' '"> {{ JSON.parse(data).bricks.description2 }} </p>
+          </div>
+          <a v-for="button in JSON.parse(data).bricks.atoms" :href="button.url" :class="[button.type, button.variant]"> {{
+            button.text }} </a>
+          <img v-for="image in JSON.parse(data).bricks.imageList" :src="[imagePrefix(preImgUrl)] + image.imageurl"
+            :alt="image.alt">
+        </div>
       </div>
-        <a v-for="button in JSON.parse(data).bricks.atoms" :href="button.url" :class="[button.type, button.variant]" > {{ button.text }} </a>
-        <img v-for="image in JSON.parse(data).bricks.imageList" :src="[imagePrefix(preImgUrl)] + image.imageurl" :alt="image.alt">
     </div>
   </div>
 </template>
