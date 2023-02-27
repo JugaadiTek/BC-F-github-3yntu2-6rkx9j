57adec358e76bedca67461c570ecf96bdac6cfad
paurush rai <paurush@modocosm.io>
2023-02-27 15:10:37 +0530

    Update wysiwygPage.vue

--- src/views/wysiwygPage.vue
+++ src/views/wysiwygPage.vue
@@ -1,12 +1,10 @@
 <script setup>
-// import { imagePrefix } from '@/utils/imgurlPrefix.js';
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 
 defineProps({
   data: { Object },
   sliceType: String,
-  preImgUrl: String,
 })
 </script>
 
@@ -17,13 +15,16 @@ defineProps({
       <div class="inner-container">
         <div class="content">
           <div class="content-block">
-            <div class="hero">
 
-              
+            <div class="hero">
+              <h2 class="title"> </h2>
+              <p class="description"> </p>
             </div>
+
             <div class="wysiwyg" v-html="JSON.parse(data)">
 
             </div>            
+
           </div>
         </div>
       </div>
