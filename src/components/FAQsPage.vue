50e24ff3eedbe024df6b954f3578c0bc27b74ca6
paurush rai <paurush@modocosm.io>
2023-02-27 10:35:12 +0530

    Create FAQsPage.vue

---
+++ src/components/FAQsPage.vue
@@ -0,0 +1,30 @@
+<script setup>
+// import { imagePrefix } from '@/utils/imgurlPrefix.js';
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+
+// defineProps({
+//   data: { Object },
+//   sliceType: String,
+//   preImgUrl: String,
+// })
+</script>
+
+<template>
+  <Header />
+  <main>
+    <div class="container">
+      <div class="inner-container">
+        <div class="content">
+          <div class="content-block">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <Footer />
+</template>
+
+
+<style scoped></style>
