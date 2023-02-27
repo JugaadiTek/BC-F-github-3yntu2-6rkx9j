113a4a4f056051310ebc5ed92a1f184d58581dbb
paurush rai <paurush@modocosm.io>
2023-02-27 15:17:23 +0530

    p

--- src/components/MC/hero.vue
+++ src/components/MC/hero.vue
@@ -11,34 +11,31 @@ defineProps({
 </script>
 <template>
   <!-- <div class="codecont">
-    <div class="codeloop">
-      <h1>Hero</h1>
-      <pre class="title">{{ JSON.parse(data) }}</pre>
-    </div>
-  </div> -->
+        <div class="codeloop">
+          <h1>Hero</h1>
+          <pre class="title">{{ JSON.parse(data) }}</pre>
+        </div>
+      </div> -->
   <!-- <code>{{ setSliceVars(herocont) }}</code> -->
   <!-- <p>{{ imagePrefix(preImgUrl) }}</p> -->
-
 
   <div class="inner-container">
     <!-- <img class="hero" :src="JSON.parse(data).heroBG" :alt="JSON.parse(data).AltText"> -->
     <div class="content">
       <div class="content-block">
         <div>
-          <span v-for="(logo, index) in JSON.parse(data).bricks.imageList" 
-            :key="index">
-          <img v-if="logo.type == 'aboveImg_logo'"
-            :src="[imagePrefix(preImgUrl)] + logo.imageurl" 
-            :alt="JSON.parse(data).bricks.AltText">
+          <span v-for="(logo, index) in JSON.parse(data).bricks.imageList" :key="index">
+            <img v-if="logo.type == 'aboveImg_logo'" :src="[imagePrefix(preImgUrl)] + logo.imageurl"
+              :alt="JSON.parse(data).bricks.AltText">
           </span>
           <h2>{{ JSON.parse(data).bricks.title }}</h2>
           <p>{{ JSON.parse(data).bricks.description }}</p>
-          <span v-for="(atom, index) in JSON.parse(data).bricks.atoms" :key="index" >
+          <span v-for="(atom, index) in JSON.parse(data).bricks.atoms" :key="index">
             <a :class="[atom.type, atom.variant, atom.styleMods]" :href="atom.url">{{ atom.text }}</a>
           </span>
         </div>
         <span v-for="(image, index) in JSON.parse(data).bricks.imageList" :key="index">
-          <img v-if="image.type== 'img_halfContent'" :src="[imagePrefix(preImgUrl)] + image.imageurl" :alt="image.alt">
+          <img v-if="image.type == 'img_halfContent'" :src="[imagePrefix(preImgUrl)] + image.imageurl" :alt="image.alt">
         </span>
       </div>
     </div>
