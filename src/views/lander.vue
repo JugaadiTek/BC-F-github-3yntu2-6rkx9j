843f16af3d410d3cadfb1610060a75da19b94e06
paurush rai <paurush@modocosm.io>
2023-02-28 18:55:54 +0530

    Update lander.vue

--- src/views/lander.vue
+++ src/views/lander.vue
@@ -2,9 +2,10 @@
 import { ref } from 'vue';
 import { directus } from '@/services/directus';
 import { RouterLink, useRoute, useRouter } from 'vue-router';
-import { getAssetURL } from '@/utils/get-asset-url';
+// import { getAssetURL } from '@/utils/get-asset-url';
 // import { o } from '../../dist/assets/app-cb1ccfc7';
-import { defineProps } from 'vue';
+// import { defineProps } from 'vue';
+// import { declareInterface } from '@babel/types';
 // import sliceLogic from '@/utils/sliceLogic.vue';
 // import QueryString from 'qs';
 // =========================importing components==================================================
@@ -55,15 +56,30 @@ async function fetchData() {
 <template>
   <main>
     <div class="container">
-      <!-- <pre>{{ lander.data }}</pre> -->
+      <!-- <pre>{{ JSON.parse(JSON.stringify(lander)) }}</pre> -->
+      <pre>{{ JSON.parse(JSON.stringify(lander.data[0])) }}</pre>
       <div class="inner-container">
-        <div class="content" >
+        
+          <!-- <div v-html="getInTouchData.wysiwyg"></div> -->
+          <!-- v-for="getInTouchData in JSON.parse(JSON.stringify(lander.data))" -->
+          
+              <div class="content-block">
+                <div class="left-column">
+                  <div class="wysiwyg" v-html="JSON.parse(JSON.stringify(lander.data[0].wysiwyg))">
+                  </div>
+                </div>
+                <div class="right-column">
+                  <div class="form" v-html="JSON.parse(JSON.stringify(lander.data[0].code))">
+                  </div>
+                  <div class="right_wysiwyg" v-html="JSON.parse(JSON.stringify(lander.data[0].right_wysiwyg))">
+                  </div>
+                </div>
+              </div>
 
-          <div v-for="getInTouchData in JSON.parse(JSON.stringify(lander.data))">
-            <div class="content-block" v-if="getInTouchData.id = '7201d5f3-800c-482c-8513-d50f69db524f'">
+          <!-- <div class="content-block" v-if="data.id = '6d9ba917-8059-494b-a8a4-c5fd7cf55449'">
               <div class="left-column">
                 <div class="wysiwyg">
-                  <div v-html="getInTouchData.wysiwyg"></div>
+                  <div v-html="data.wysiwyg"></div>
                 </div>
               </div>
               <div class="right-column">
@@ -74,39 +90,23 @@ async function fetchData() {
                 </div>
               </div>
             </div>
-          </div>
 
-          <!-- <div class="content-block" v-if="data.id = '6d9ba917-8059-494b-a8a4-c5fd7cf55449'">
-            <div class="left-column">
-              <div class="wysiwyg">
-                <div v-html="data.wysiwyg"></div>
+            <div class="content-block" v-if="data.id = 'dc16b12d-3855-4533-a4f4-1e68afef445b'">
+              <div class="left-column">
+                <div class="wysiwyg">
+                  <div v-html="data.wysiwyg"></div>
+                </div>
               </div>
-            </div>
-            <div class="right-column">
-              <div class="form">
+              <div class="right-column">
+                <div class="form">
 
+                </div>
+                <div class="wysiwyg">
+                </div>
               </div>
-              <div class="wysiwyg">
-              </div>
-            </div>
-          </div>
+            </div> -->
 
-          <div class="content-block" v-if="data.id = 'dc16b12d-3855-4533-a4f4-1e68afef445b'">
-            <div class="left-column">
-              <div class="wysiwyg">
-                <div v-html="data.wysiwyg"></div>
-              </div>
-            </div>
-            <div class="right-column">
-              <div class="form">
-
-              </div>
-              <div class="wysiwyg">
-              </div>
-            </div>
-          </div> -->
-
-        </div>
+        
       </div>
     </div>
   </main>
